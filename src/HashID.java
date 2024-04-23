e2815900a025eb95a981e26efa0de25ce8b1fc24
Ihsaan-Rashid <Ihsaan.Rashid@city.ac.uk>
2024-04-23 15:03:36 +0100

    Changed HashID class as everything that was in this class, is implemented within FullNode.java now.

--- src/HashID.java
+++ src/HashID.java
@@ -21,38 +21,4 @@ public class HashID {
 			throw new Exception("No new line at the end of input to HashID");
 		}
 	}
-	//this method will calculate the distance between two hashIDs by counting the number of differing bits
-	public static int calculateDistanceBetween(byte[] hashID1, byte[] hashID2) {
-		int distance = 0;
-		for (int i = 0; i < hashID1.length; i++) {
-			distance += Integer.bitCount(hashID1[i] ^ hashID2[i]);
-		}
-		return 256 - distance;
-	}
-	// New method to count leading zeros in a byte array. It counts the number of leading zeros in a hashID
-	public static int countAmountOfZeros(byte[] hashID) {
-		int leadingZeros = 0;
-		for (byte b : hashID) {
-			if (b == 0) {
-				leadingZeros += 8; // Each byte contains 8 bits
-			} else {
-				leadingZeros += Integer.numberOfLeadingZeros(Byte.toUnsignedInt(b)) - 24;
-				break;
-			}
-		}
-		return leadingZeros;
-	}
-	// New method to perform a bitwise XOR operation between two hashIDs, returning the result as a new byte array
-	public static byte[] xor(byte[] hashID1, byte[] hashID2) {
-		if (hashID1.length != hashID2.length) {
-			throw new IllegalArgumentException("Byte arrays must have the same length for XOR operation");
-		}
-
-		byte[] result = new byte[hashID1.length];
-		for (int i = 0; i < hashID1.length; i++) {
-			result[i] = (byte) (hashID1[i] ^ hashID2[i]);
-		}
-		return result;
-	}
 }
-
